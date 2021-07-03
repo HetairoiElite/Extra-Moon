560becd5c4f83b55370526b27ed021f2072b9d8c
Hetairoi <jonathan90090@gmail.com>
2021-07-03 16:05:01 -0500

    Clases agregadas

---
+++ Ejercicio1.cpp
@@ -0,0 +1,50 @@
+class Empleado
+{
+private:
+    /* data */
+public:
+    Empleado();
+    ~Empleado();
+};
+
+Empleado::Empleado()
+{
+}
+
+Empleado::~Empleado()
+{
+}
+
+class EmpleadoxHora : public Empleado
+{
+private:
+    /* data */
+public:
+    EmpleadoxHora();
+    ~EmpleadoxHora();
+};
+
+EmpleadoxHora::EmpleadoxHora()
+{
+}
+
+EmpleadoxHora::~EmpleadoxHora()
+{
+}
+
+class EmpleadoxPlanta : public Empleado
+{
+private:
+    /* data */
+public:
+    EmpleadoxPlanta();
+    ~EmpleadoxPlanta();
+};
+
+EmpleadoxPlanta::EmpleadoxPlanta()
+{
+}
+
+EmpleadoxPlanta::~EmpleadoxPlanta()
+{
+}
