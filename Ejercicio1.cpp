f2c67a0f85d41c641bc9323985dce8299d973fde
Hetairoi <jonathan90090@gmail.com>
2021-07-05 14:55:46 -0500

    Correciones

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -111,9 +111,9 @@ EmpleadoxPlanta::~EmpleadoxPlanta()
 int main(int argc, const char **argv)
 {
     EmpleadoxPlanta e1(10.10, "John", HorasxJornada);
-    std::cout << "El suledo del empleado de planta " << e1.getNombre() << " es: " << e1.calcularSueldo() << std::endl;
+    std::cout << "El sueldo del empleado de planta " << e1.getNombre() << " es: " << e1.calcularSueldo() << std::endl;
 
     EmpleadoxHora e2(5.50, "Gato", HorasxJornada);
-    std::cout << "El suledo del empleado por hora " << e2.getNombre() << " es: " << e2.calcularSueldo() << std::endl;
+    std::cout << "El sueldo del empleado por hora " << e2.getNombre() << " es: " << e2.calcularSueldo() << std::endl;
     return 0;
 }
